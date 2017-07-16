d50b29b82eb257c14a335b6e23e225720417541b
System <none>
2017-07-16 03:39:44 +0000

    ❄️🚏 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# timestamp
+###Timestamp microservice
+
+Append a human readable or Unix timestamp to the url and receive a 
+Unix and human readable timestamp back.
+
+Application is running at `https://delicious-detail.glitch.me/`
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const dateParser = require('./date-parser')
+
+app.use(express.static('public'))
+
+app.get('/*', (req, res) => {
+  let date = dateParser(req)
+  res.send(date)
+})
+
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
